e0cebc614206d660bc95253d134a324a46852df7
angolosFlame <couragetkordzi@gmail.com>
2022-12-17 06:44:57 -0800

    a function that checks for alphabetic character

---
+++ 0x02-functions_nested_loops/4-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**this function checks if a charactr is alphabetic
+ * returns 1 if c is a letter, lower || upper
+ * returns 0 if c is not a letter
+ */
+
+int main(void)
+{
+	int r;
+
+	r = _isalpha('H');
+	_putchar(r + '0');
+	r = _isalpha('o');
+	_putchar(r + '0');
+	r = _isalpha(108);
+	_putchar(r + '0');
+	r = _isalpha(';');
+	_putchar(r + '0');
+	_putchar('\n');
+	return (0);
+}
