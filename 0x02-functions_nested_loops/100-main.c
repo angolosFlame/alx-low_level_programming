dcad1fcd218057a04e7b6858f0d15ac6c09e94be
angolosFlame <couragetkordzi@gmail.com>
2022-12-18 15:52:54 -0800

    A function that prints n times table starting with 0

---
+++ 0x02-functions_nested_loops/100-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/** A funtion that prints n times table
+ * starting from 0
+ * if n is less than 15 or 0, the would not print anything
+ */
+
+int main(void)
+{
+	print_times_table(3);
+	_putchar('\n');
+	print_times_table(5);
+	_putchar('\n');
+	print_times_table(98);
+	_putchar('\n');
+	print_times_table(12);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/** A function that printn times table 
+ * starting with 0
+ */
+
+void print_times_table(int n)
+{
+	int i, j, k;
+
+	if (n > 0 && n < 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('0');
+			for (j = 1; j <= n; j++)
+			{
+				k = i * j;
+				_putchar(',');
+				_putchar(' ');
+				if (k <= 9)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				}
+				else if (k <= 99)
+				{
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+				else
+				{
+					_putchar(((k / 100) % 10) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
