ef019f11d7ae69cc6caed1f5461942f0e9f8b3b4
angolosFlame <couragetkordzi@gmail.com>
2022-12-22 10:56:05 -0800

    more pointers and strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * A function that concatenates
+ * two strings
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int a = -1, i;
+	for (i = 0; dest[i] !='\0'; i++);
+
+	do {
+		a++;
+		dest[i] = src[a];
+		i++;
+	} while (src[a] != '\0');
+
+	return (dest);
+}
+
