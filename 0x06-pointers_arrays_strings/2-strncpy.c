2a68611527438eed036b67935a9a873a39e93660
angolosFlame <couragetkordzi@gmail.com>
2022-12-23 07:17:51 -0800

    more pinters, arrys and string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * A function that copies a string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int srclen = 0, i = 0;
+	char *temp = dest, *start = src;
+
+	while (*src)
+	{
+		srclen++;
+		src++;
+	}
+	srclen++;
+	if (n > srclen)
+	{
+		n = srclen;
+	}
+	src = start;
+	for (; i < n; i++)
+	{
+		*dest++ = *src++;
+	}
+	return (temp);
+}
---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * A function to compare two strings
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++, s2++;
+	}
+	return (*s1 - *s2);
+}
+
