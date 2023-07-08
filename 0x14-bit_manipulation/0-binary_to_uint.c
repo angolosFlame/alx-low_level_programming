de2ef1dc1613f67f9399e95e54d73a5e209ba3f1
anglosFlame <couragetkordzi@gmail.com>
2023-07-07 20:38:38 -0700

    coverts binary number to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,23 +10,16 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int base = 1, result = 0, len = 0;
+	unsigned int nu = 0;
+	int i = 0;
 
 	if (b == NULL)
-		return (0);
-
-	while (b[len])
-		len++;
-
-	while (len)
+		return 0;
+	while (b[i] == '0' || b[i] == '1')
 	{
-		if (b[len -1] != '0' && b[len -1] != '1')
-			return (0);
-
-		if (b[len -1] == '1')
-			result += base;
-		base*= 2;
-		len--;
-	}
-	return (result);
+		nu <<= 1;
+		nu += b[i] - '0';
+		i++;
+	}	
+	return (nu);
 }
